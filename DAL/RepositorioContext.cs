c0a0a2653185d1e1502577d46e451905f90b0723
Letícia Paulino <karlaleticiatavares@gmail.com>
2021-05-24 00:04:09 -0300

    Cria classe RepositorioContext

---
+++ DAL/RepositorioContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class RepositorioContext : DbContext
+    {
+        public RepositorioContext() : base() { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(connectionString: @"server = localhost; user id = root; pwd = batata; database = duas-camadas;");
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
